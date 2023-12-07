36cab4834b8af7dc3fb561ceb79405cbbe49c036
satyaazure <bindumadhavib1992@gmail.com>
2023-12-07 09:39:13 +0100

    Add person class

---
+++ src/main/java/se/lexicon/Person.java
@@ -0,0 +1,29 @@
+package se.lexicon;
+
+public class Person {
+    private int personId;
+    private String firstName;
+    private String lastName;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

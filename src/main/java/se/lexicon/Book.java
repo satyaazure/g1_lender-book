b517b3e564e309d1a7f381d52252192a93da0edd
satyaazure <bindumadhavib1992@gmail.com>
2023-12-07 09:56:31 +0100

    Add Book class

---
+++ src/main/java/se/lexicon/Book.java
@@ -0,0 +1,25 @@
+package se.lexicon;
+
+public class Book {
+    private String title;
+
+    private String author;
+    private boolean available;
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
